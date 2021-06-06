058aa4327d117ef9b8de78aacb4edba33b945f69
bunshinn <bunshinn@126.com>
2021-06-06 19:40:56 +0800

    m

--- static/js/track.js
+++ static/js/track.js
@@ -2,7 +2,7 @@
 var _tst = _tst || [];
 (function() {
   var ts = document.createElement("script");
-  ts.src = "http://localhost:1313/js/ts.js?bs-hugo-blog";
+  ts.src = "js/ts.js?bs-hugo-blog";
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(ts, s);
 })();
